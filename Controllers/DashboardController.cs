684a7cd062251d4d59aa3d6e10594353ed105490
Nirob-Barman <niroborin111@gmail.com>
2025-04-26 21:02:12 +0600

    DashBoard Controller

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EliteSportsAcademy.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        // User-specific dashboard
+        [Authorize(Roles = "User")]
+        public IActionResult UserDashboard()
+        {
+            return View();
+        }
+
+        // Instructor-specific dashboard
+        [Authorize(Roles = "Instructor")]
+        public IActionResult InstructorDashboard()
+        {
+            return View();
+        }
+
+        // Admin-specific dashboard
+        [Authorize(Roles = "Admin")]
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
