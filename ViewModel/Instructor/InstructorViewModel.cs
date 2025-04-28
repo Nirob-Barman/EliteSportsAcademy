8184b66abe747ecd868505b1ca51f8e248b13709
Nirob-Barman <niroborin111@gmail.com>
2025-04-28 08:05:13 +0600

    Instructor View Model Add

---
+++ ViewModel/Instructor/InstructorViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace EliteSportsAcademy.ViewModel.Instructor
+{
+    public class InstructorViewModel
+    {
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? PhotoURL { get; set; }
+        public List<string>? ClassesTaken { get; set; }
+    }
+}
